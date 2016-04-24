0499d4b3f590a1d29ae9a675bd89fba661fcd3f6
areag <areagsa@gmail.com>
2016-04-24 15:48:34 -0300

    existencias

--- web/js/js_gananciasGan.js
+++ web/js/js_gananciasGan.js
@@ -8,39 +8,42 @@
 function verificarVacios() {
 
     var fecha_desde = document.getElementById("fecha_desde").value;
-    var dia_desde = fecha_desde.substr(0 , 2);
-    var mes_desde = fecha_desde.substr(3 , 2);
+    var dia_desde = fecha_desde.substr(0, 2);
+    var mes_desde = fecha_desde.substr(3, 2);
     var año_desde = fecha_desde.substr(6, 4);
-  
+
     var fecha_hasta = document.getElementById("fecha_hasta").value;
-     var dia_hasta = fecha_hasta.substr(0 , 2);
-    var mes_hasta = fecha_hasta.substr(3 , 2);
+    var dia_hasta = fecha_hasta.substr(0, 2);
+    var mes_hasta = fecha_hasta.substr(3, 2);
     var año_hasta = fecha_hasta.substr(6, 4);
 
-
-    if (fecha_desde == 0 || fecha_hasta == 0) {
-        alert("Complete los campos Obligatorios!");
-    } else if (año_desde > año_hasta) {
-        alert("La fecha de inicio no puede ser mayor que la final");
-    }else if(año_desde == año_hasta){
-        if(mes_desde > mes_hasta){
-            alert("La fecha de inicio no puede ser mayor que la final");
-        }else if (mes_desde == mes_hasta){
-            if (dia_desde > dia_hasta){
-                alert("La fecha de inicio no puede ser mayor que la final");
-            }else{
-                alert ("funciona");
-                document.miformGan.submit();
+    if (fecha_desde.length != 0 || fecha_hasta.length != 0) {
+        if (año_desde > año_hasta) {
+            alert("La fecha de inicio no puede ser mayor que la final 1");
+        } else if (año_desde < año_hasta) {
+            alert("funciona");
+            newwindow = window.open("ResulGananciasGan.jsp?&fecha_desde=" + fecha_desde + "&fecha_hasta=" + fecha_hasta, "central");
+        } else if (año_desde == año_hasta) {
+            if (mes_desde > mes_hasta) {
+                alert("La fecha de inicio no puede ser mayor que la final 2 ");
+            } else if (mes_desde < mes_hasta) {
+            alert("funciona");
+            newwindow = window.open("ResulGananciasGan.jsp?&fecha_desde=" + fecha_desde + "&fecha_hasta=" + fecha_hasta, "central");
+        } else if (mes_desde == mes_hasta) {
+                if (dia_desde > dia_hasta) {
+                    alert("La fecha de inicio no puede ser mayor que la final 3");
+                } else if (dia_desde <= dia_hasta) {
+                    alert("funciona");
+                    newwindow = window.open("ResulGananciasGan.jsp?&fecha_desde=" + fecha_desde + "&fecha_hasta=" + fecha_hasta, "central");
+                }
             }
         }
+    } else {
+        alert("Complete los campos Obligatorios!");
     }
-        
-   
-   
-        
-   
-
-
-
-
 }
+
+
+
+
+
